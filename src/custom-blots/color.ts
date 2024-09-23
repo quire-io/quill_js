6e648d54648a530f29ce6ddecb177fbdd5a57c31
Rudy Huang <rudyhuang@potix.com>
2024-09-23 18:50:45 +0800

    Color: add whitelist

--- src/custom-blots/color.ts
+++ src/custom-blots/color.ts
@@ -1,7 +1,19 @@
 import { ClassAttributor, Scope } from 'parchment';
 
+const textColorRowSpan = 4;
+const textColorColSpan = 8;
+
+function* textColors() {
+  for (var i = 0; i < textColorRowSpan; i++) {
+    for (var j = 0; j < textColorColSpan; j++) {
+      yield `${i}${j}`;
+    }
+  }
+}
+
 const ColorClass = new ClassAttributor('color', 'textcr', {
   scope: Scope.INLINE,
+  whitelist: [...textColors()],
 });
 
 export default ColorClass;
