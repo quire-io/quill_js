bb84a99fe4e0fa93cc3005d3b8690d9e2c7a678e
jimmyshiau <jimmyshiau73@gmail.com>
2025-02-14 09:04:09 +0800

    #21448: Not allow insert text to embed blot

--- src/custom-blots/phone.ts
+++ src/custom-blots/phone.ts
@@ -9,7 +9,7 @@ class PhoneBlot extends EmbedBlot {
     static create(value: string) {
         const node = super.create() as HTMLAnchorElement;
         
-        // node.setAttribute('contenteditable', 'false');
+        node.setAttribute('contenteditable', 'false');
         node.setAttribute('target', '_blank');
 
         PhoneBlot._updateNode(node, value);
