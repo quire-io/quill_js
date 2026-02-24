445b64afe850d15ea8b175965da8bf0cc634b841
jimmyshiau <jimmyshiau73@gmail.com>
2026-02-24 16:11:34 +0800

    Fix The offset 4294967295 is larger than the Text node's length

--- src/custom-blots/text.ts
+++ src/custom-blots/text.ts
@@ -4,10 +4,15 @@ import TextBlot from 'quill/blots/text';
 class Text extends TextBlot {
     
     split(index: number, force?: boolean): Blot | null {
-        if (index > this.length()) {
-            return this.next;
+        if (index >= this.length()) {
+            return this.next;//#24088: the maximum value for a 32-bit unsigned integer
         }
-        return super.split(index, force);
+
+        try {
+            return super.split(index, force);
+        } catch (e) {
+            return this.next;//#24088: the maximum value for a 32-bit unsigned integer
+        }
     }
 }
 
